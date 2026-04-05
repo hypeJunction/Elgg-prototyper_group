1e23e0e4452f690f8d0a7caea525f47bebff3c5b
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-04-05 13:38:49 +0200

    migrate(3.x): automated AST transformations
    Applied by elgg-migrate automated rules.
    See elgg-migrate rules/2x-to-3x/manifest.json for details.

--- views/default/input/groups/owner.php
+++ views/default/input/groups/owner.php
@@ -2,40 +2,23 @@
 
 $entity = elgg_extract('entity', $vars);
 if (!$entity instanceof ElggGroup || !$entity->guid) {
-	return;
+    return;
 }
-
 if ($entity->owner_guid != elgg_get_logged_in_user_guid() && !elgg_is_admin_logged_in()) {
-	return;
+    return;
 }
-
 $members = [];
-
 $dbprefix = elgg_get_config('dbprefix');
-$options = [
-	"type" => "user",
-	"relationship" => "member",
-	"relationship_guid" => $entity->getGUID(),
-	"inverse_relationship" => true,
-	"limit" => false,
-	"callback" => false,
-	"joins" => ["JOIN {$dbprefix}users_entity ue ON e.guid = ue.guid"],
-	"selects" => ['ue.*'],
-	"order_by" => 'ue.name ASC',
-];
-
-$batch = new ElggBatch("elgg_get_entities_from_relationship", $options);
+$options = ["type" => "user", "relationship" => "member", "relationship_guid" => $entity->getGUID(), "inverse_relationship" => true, "limit" => false, "callback" => false, "joins" => ["JOIN {$dbprefix}users_entity ue ON e.guid = ue.guid"], "selects" => ['ue.*'], "order_by" => 'ue.name ASC'];
+$batch = new ElggBatch("elgg_get_entities", $options);
 foreach ($batch as $member) {
-	$option_text = "$member->name (@$member->username)";
-	$members[$member->guid] = htmlspecialchars($option_text, ENT_QUOTES, "UTF-8", false);
+    $option_text = "{$member->name} (@{$member->username})";
+    $members[$member->guid] = htmlspecialchars($option_text, ENT_QUOTES, "UTF-8", false);
 }
-
 $vars['options_values'] = $members;
 $vars['id'] = 'groups-owner-guid';
 $vars['class'] = 'groups-owner-input';
-
 echo elgg_view('input/select', $vars);
-
 if ($entity->owner_guid == elgg_get_logged_in_user_guid()) {
-	echo "<span class='elgg-text-help'>" . elgg_echo("groups:owner:warning") . "</span>";
+    echo "<span class='elgg-text-help'>" . elgg_echo("groups:owner:warning") . "</span>";
 }
